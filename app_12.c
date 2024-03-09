a6a1f57a9910d27b6a3dbcd757a64ff4721fe127
MohamedMahmoudH <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-03-10 01:52:54 +0200

    Add files via upload

---
+++ app_12.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <math.h>
+
+#define SPACE ' '
+
+/*200 min*/
+
+int main()
+
+{
+        int maxNum        = 0;
+        int currentRow    = 0;
+        int currentColumn = 0;
+        int carrier       = 0;
+        int digitNum      = 0;
+        int digitCarrier  = 0;
+        int ii            = 0;
+        int jj            = 0;
+
+        printf("enter maximum number of square : ");
+        scanf ("%d",&maxNum);
+
+        carrier  = maxNum ;
+        digitNum = floor(log10(maxNum))+1;
+
+
+        for(currentRow = 1 ; currentRow <= ((maxNum*2)-1) ; currentRow++)
+        {
+            for(currentColumn = 1 ; currentColumn <= ((maxNum*2)-1) ; currentColumn++)
+            {
+                if(((currentRow + currentColumn)% 2) == 1) //if any of these odd the program will print space
+                {
+                    digitCarrier = floor(log10(carrier))+1;
+                    for(ii = 1 ; ii <= (digitNum - digitCarrier + 1) ; ii++)// to avoid error that producing by tens and hundreds
+                    {
+                        printf("%c",SPACE);
+                        /*
+                        if(digitCarrier == 2)
+                        {
+                            //printf("%c%c%c",SPACE,SPACE,SPACE);
+                            //printf("%c%c",SPACE,SPACE);
+                            //printf("%c",SPACE);
+                        }
+                        else if(digitCarrier == 1)
+                        {
+                            //printf("%c%c%c",SPACE,SPACE,SPACE);
+                            //printf("%c%c",SPACE,SPACE);
+                            //printf("%c",SPACE);
+                        }
+                        */
+                    }
+                }
+                else
+                {
+                    if(currentRow <= maxNum)
+                    {
+                        if((currentColumn >= currentRow) && (currentColumn <= (carrier + maxNum -1)))
+                        {
+                            printf("%d ",carrier);
+                        }
+                        else
+                        {
+                            for(jj = 1 ; jj < maxNum ; jj++)
+                            {
+                                if(currentColumn == jj)
+                                {
+                                    printf("%d ",(maxNum - jj + 1));
+                                }
+                                else if(currentColumn == ((2* maxNum)- jj))
+                                {
+                                    printf("%d ",(maxNum - jj + 1));
+                                }
+                            }
+                        }
+                    }
+                    else if((currentRow <= ((maxNum*2)-1)) && (currentRow > maxNum))
+                    {
+                        if((currentColumn <= currentRow) && (currentColumn >= (currentRow-2*(carrier -1))))
+                        {
+                            printf("%d ",carrier);
+                        }
+                        else
+                        {
+                            for(jj = 1 ; jj < maxNum ; jj++)
+                            {
+                                if(currentColumn == jj)
+                                {
+                                    printf("%d ",(maxNum - jj + 1));
+                                }
+                                else if(currentColumn == ((2* maxNum)- jj))
+                                {
+                                    printf("%d ",(maxNum - jj + 1));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            printf("\n");
+            if(currentRow < maxNum)
+            {
+                carrier-- ;
+            }
+            else
+            {
+                carrier++ ;
+            }
+        }
+}
