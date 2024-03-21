7db555cf7854a43e58b443ea443a39133b26565b
Mohamed M. Hassan <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-03-22 01:09:50 +0200

    Update app_11.c
    1- user can input the symbol 
    2- limit symbol to one symbol input
    3- reduce size of code by using function

--- app_11.c
+++ app_11.c
@@ -8,36 +8,43 @@ draw hollow square star pattern with diagonal
 
 /*40 min*/
 
+void print_shape_limits(void);// Print the top or bottom row
+
+int  rowsNumber    ;
+int  currentRow    ;
+int  currentColumn ;
+int  counter       ;
+char symbol        ;
+
 int main()
 {
-    int rowsNumber    = 0;
-    int currentRow    = 0;
-    int currentColumn = 0;
-    int counter       = 0;
-
-
     printf("enter rows number : ");
     scanf ("%d",&rowsNumber);
+    printf("enter only one symbol : ");
+    scanf (" %c",&symbol);//the space before %c to clear the buffer
+
+    if(getchar() != '\n')//to get from user only one symbol
+    {
+        printf("Invalid input , Please enter only one symbol\n");
+        return 1;
+    }
 
     if(rowsNumber <= 4)//input validation
     {
-        printf("ERROR\nyou can't draw a hollow square star pattern with diagonal by using less than 4 * ");
+        printf("ERROR\nyou can't draw a hollow square star pattern with diagonal by using less than 5 rows");
     }
     else
     {
-        for(counter = 1 ; counter <= rowsNumber ; counter++)// Print the top row
-        {
-            printf("*%c",SPACE);
-        }
+        print_shape_limits();//top
         printf("\n");
 
-        for(currentRow = 2 ; currentRow < rowsNumber ; currentRow++)// Print the body of the pattern
+        for(currentRow = 2 ; currentRow < rowsNumber ; ++currentRow)// Print the body of the pattern
         {
-            for(currentColumn = 1 ; currentColumn <= rowsNumber ; currentColumn++)
+            for(currentColumn = 1 ; currentColumn <= rowsNumber ; ++currentColumn)
             {
                 if(currentColumn == 1 || currentColumn == currentRow || currentColumn == (rowsNumber - currentRow + 1 )|| currentColumn == rowsNumber)
                 {
-                    printf("*%c",SPACE);
+                    printf("%c%c",symbol,SPACE);
                 }
                 else
                 {
@@ -47,10 +54,14 @@ int main()
             }
             printf("\n");
         }
-        for(counter = 1 ; counter <= rowsNumber ; counter++)// Print the bottom row
-        {
-            printf("*%c",SPACE);
-        }
+        print_shape_limits();//bottom
     }
     return 0;
+}
+void print_shape_limits(void)
+{
+    for(counter = 1 ; counter <= rowsNumber ; ++counter)
+    {
+        printf("%c%c",symbol,SPACE);
+    }
 }
