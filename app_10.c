c40a3b3d7c11e40b33f9a9045f26eb0846aecd29
Mohamed M. Hassan <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-03-21 03:49:55 +0200

    Update app_10.c
    user can input any symbol

--- app_10.c
+++ app_10.c
@@ -1,5 +1,5 @@
 /*
-write a code that print number of "*" pyramids and take from user number of rows and number of pyramids
+write a code that print number of pyramids and take from user number of rows and number of pyramids and the symbol
 */
 
 #include <stdio.h>
@@ -10,16 +10,19 @@ write a code that print number of "*" pyramids and take from user number of rows
 
 int main()
 {
-    int rows_num       = 0;
-    int pyramids_num   = 0;
-    int current_row    = 0;
-    int current_column = 0;
-    int counter        = 0;
-    int counter_2      = 0; // to avoid infinite loop "logical error"
+    int  rows_num       = 0;
+    int  pyramids_num   = 0;
+    int  current_row    = 0;
+    int  current_column = 0;
+    int  counter        = 0;
+    int  counter_2      = 0; // to avoid infinite loop "logical error"
+    char symbol         = 0;
 
 
     printf("enter rows number then pyramids number : ");
     scanf ("%d %d",&rows_num,&pyramids_num);
+    printf("enter the symbol : ");
+    scanf (" %c",&symbol);
 
 
     if(rows_num <= 0 && pyramids_num <= 0)// input validation
@@ -28,37 +31,37 @@ int main()
     }
     else
     {
-        for(current_row = 1 ; current_row <= rows_num ; current_row++)
+        for(current_row = 1 ; current_row <= rows_num ; ++current_row)
         {
-            for(current_column = 1 ; current_column <= (rows_num) ; current_column++)
+            for(current_column = 1 ; current_column <= (rows_num) ; ++current_column)
             {
                 if(current_column == (rows_num - current_row + 1))
                 {
-                    for(counter = 1 ; counter < current_row ; counter++)
+                    for(counter = 1 ; counter < current_row ; ++counter)
                     {
-                        printf("*%c",SPACE);
+                        printf("%c%c",symbol,SPACE);
                     }
-                    printf("*");
+                    printf("%c",symbol);
 
                 }
-                else if(current_column <= (rows_num - current_row + 1))//to delete all spaces after the pyramid
+                else if(current_column <= (rows_num - current_row + 1))//to avoid printing unwanted spaces after the pyramid
                 {
                     printf("%c",SPACE);
                 }
             }
 
-            for(counter_2 = 1 ; counter_2 < pyramids_num ; counter_2++)
+            for(counter_2 = 1 ; counter_2 < pyramids_num ; ++counter_2)
             {
-                for(counter = 1 ; counter <= ((2* rows_num) - ((2* current_row) - 1)); counter++)
+                for(counter = 1 ; counter <= ((2* rows_num) - ((2* current_row) - 1)); ++counter)
                 {
                     printf("%c",SPACE);
                 }
 
-                for(counter = 1 ; counter < current_row ; counter++)
+                for(counter = 1 ; counter < current_row ; ++counter)
                 {
-                    printf("*%c",SPACE);
+                    printf("%c%c",symbol,SPACE);
                 }
-                printf("*");
+                printf("%c",symbol);
             }
 
             printf("\n");
