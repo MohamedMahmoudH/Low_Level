7b364c43b53b4692c9b962f0894366f1c7e3db8b
Mohamed M. Hassan <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-03-23 03:01:24 +0200

    Update app_12.c
    1- solving the errors
    2- using functions 
    3- make the code easy for readability

--- app_12.c
+++ app_12.c
@@ -1,117 +1,140 @@
 /*
+for 1<=n<1000
 Print a pattern of numbers from 1 to maxNum as shown below
-if input = 7;
+if input = 15;
 
 output :
-7   7   7   7   7   7   7
-  6   6   6   6   6   6
-7   5   5   5   5   5   7
-  6   4   4   4   4   6
-7   5   3   3   3   5   7
-  6   4   2   2   4   6
-7   5   3   1   3   5   7
-  6   4   2   2   4   6
-7   5   3   3   3   5   7
-  6   4   4   4   4   6
-7   5   5   5   5   5   7
-  6   6   6   6   6   6
-7   7   7   7   7   7   7
+15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15
+15  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  15
+15  14  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  14  15
+15  14  13  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  13  14  15
+15  14  13  12  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  12  13  14  15
+15  14  13  12  11  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  11  12  13  14  15
+15  14  13  12  11  10   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   8   8   8   8   8   8   8   8   8   8   8   8   8   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   7   7   7   7   7   7   7   7   7   7   7   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   6   6   6   6   6   6   6   6   6   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   5   5   5   5   5   5   5   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   4   4   4   4   4   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   3   3   3   3   3   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   3   2   2   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   3   2   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   3   2   2   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   3   3   3   3   3   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   4   4   4   4   4   4   4   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   5   5   5   5   5   5   5   5   5   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   6   6   6   6   6   6   6   6   6   6   6   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   7   7   7   7   7   7   7   7   7   7   7   7   7   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   8   8   8   8   8   8   8   8   8   8   8   8   8   8   8   9  10  11  12  13  14  15
+15  14  13  12  11  10   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9   9  10  11  12  13  14  15
+15  14  13  12  11  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  10  11  12  13  14  15
+15  14  13  12  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  11  12  13  14  15
+15  14  13  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  12  13  14  15
+15  14  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  13  14  15
+15  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  14  15
+15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15  15
 */
-
 #include <stdio.h>
 #include <math.h>
 
 #define SPACE ' '
 
 /*200 min*/
+/*70  min*/ //to update the code
+
+void solve_space_issue(void);
+void side_two_triangles(void);
+
+int maxNum        ;
+int currentRow    ;
+int currentColumn ;
+int carrier       ;
+int digitNum      ;
+int ii            ;
+int jj            ;
 
 int main()
 
 {
-        int maxNum        = 0;
-        int currentRow    = 0;
-        int currentColumn = 0;
-        int carrier       = 0;
-        int digitNum      = 0;
-        int digitCarrier  = 0;
-        int ii            = 0;
-        int jj            = 0;
+    printf("enter maximum number of square : ");
+    scanf ("%d",&maxNum);
 
-        printf("enter maximum number of square : ");
-        scanf ("%d",&maxNum);
-
-        carrier  = maxNum ;
-        digitNum = floor(log10(maxNum))+1;
+    carrier  = maxNum ;
+    digitNum = floor(log10(maxNum))+1;
 
 
-        for(currentRow = 1 ; currentRow <= ((maxNum*2)-1) ; currentRow++)
+    for(currentRow = 1 ; currentRow <= ((maxNum*2)-1) ; ++currentRow)
+    {
+        for(currentColumn = 1 ; currentColumn <= ((maxNum*2)-1) ; ++currentColumn)
         {
-            for(currentColumn = 1 ; currentColumn <= ((maxNum*2)-1) ; currentColumn++)
+            if(currentRow <= maxNum)
             {
-                if(((currentRow + currentColumn)% 2) == 1) //if any of these odd the program will print space
+                if((currentColumn >= currentRow) && (currentColumn <= (carrier + maxNum -1)))
                 {
-                    digitCarrier = floor(log10(carrier))+1;
-                    for(ii = 1 ; ii <= (digitNum - digitCarrier + 1) ; ii++)// to avoid error that producing by tens and hundreds
-                    {
-                        printf("%c%c",SPACE,SPACE);
-                    }
+                    solve_space_issue();
                 }
                 else
                 {
-                    if(currentRow <= maxNum)
-                    {
-                        if((currentColumn >= currentRow) && (currentColumn <= (carrier + maxNum -1)))
-                        {
-                            printf("%d ",carrier);
-                        }
-                        else
-                        {
-                            for(jj = 1 ; jj < maxNum ; jj++)
-                            {
-                                if(currentColumn == jj)
-                                {
-                                    printf("%d ",(maxNum - jj + 1));
-                                }
-                                else if(currentColumn == ((2* maxNum)- jj))
-                                {
-                                    printf("%d ",(maxNum - jj + 1));
-                                }
-                            }
-                        }
-                    }
-                    else if((currentRow <= ((maxNum*2)-1)) && (currentRow > maxNum))
-                    {
-                        if((currentColumn <= currentRow) && (currentColumn >= (currentRow-2*(carrier -1))))
-                        {
-                            printf("%d ",carrier);
-                        }
-                        else
-                        {
-                            for(jj = 1 ; jj < maxNum ; jj++)
-                            {
-                                if(currentColumn == jj)
-                                {
-                                    printf("%d ",(maxNum - jj + 1));
-                                }
-                                else if(currentColumn == ((2* maxNum)- jj))
-                                {
-                                    printf("%d ",(maxNum - jj + 1));
-                                }
-                            }
-                        }
-                    }
+                    side_two_triangles();
                 }
             }
-
-
-            printf("\n");
-            if(currentRow < maxNum)
+            else if((currentRow <= ((maxNum*2)-1)) && (currentRow > maxNum))
             {
-                carrier-- ;
-            }
-            else
-            {
-                carrier++ ;
+                if((currentColumn <= currentRow) && (currentColumn >= (currentRow-2*(carrier -1))))
+                {
+                    solve_space_issue();
+                }
+                else
+                {
+                    side_two_triangles();
+                }
             }
         }
+
+        printf("\n");
+        if(currentRow < maxNum)
+        {
+            carrier-- ;
+        }
+        else
+        {
+            carrier++ ;
+        }
+    }
+}
+
+void solve_space_issue(void)
+{
+    switch(digitNum)
+    {
+        case 3:
+            printf("%3d   ",carrier);
+            break;
+        case 2:
+            printf("%2d  ",carrier);
+            break;
+        case 1:
+            printf("%d ",carrier);
+    }
+}
+
+void side_two_triangles(void)
+{
+    for(jj = 1 ; jj < maxNum ; jj++)
+    {
+        if((currentColumn == jj) || (currentColumn == ((2* maxNum)- jj)))
+        {
+            switch(digitNum)
+            {
+                case 3:
+                    printf("%3d   ",(maxNum - jj + 1));
+                    break;
+                case 2:
+                    printf("%2d  ",(maxNum - jj + 1));
+                    break;
+                case 1:
+                    printf("%d ",(maxNum - jj + 1));
+            }
+        }
+    }
 }
